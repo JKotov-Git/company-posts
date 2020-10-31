162d246f39c43d00c63a4832b079783da0d39229
Jassen Kotov <jassen.kotov@gmail.com>
2020-10-31 20:28:07 +0200

    create account page

--- client/src/pages/CreateAccount/CreateAccount.js
+++ client/src/pages/CreateAccount/CreateAccount.js
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./CreateAccount.css";
+
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const generateRandomId = () => {
+  let genId = Math.floor(Math.random() * 100);
+  return genId;
+};
 
 const CreateAccount = () => {
-    return (
-        <div>
-            <h1>Create Account</h1>
-        </div>
-    )
-}
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
+  return (
+    <div className="create-account">
+      <div className="create-account-container">
+        <h1>Create Account</h1>
+        <form className="create-account-form">
+          <label>Username</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <label>Email</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label>Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label>Re-enter password</label>
+          <input
+            type="password"
+            value={rePassword}
+            onChange={(e) => setRePassword(e.target.value)}
+          />
 
-export default CreateAccount
+          <button type="button"
+        //    onClick={createAccount}
+           >
+            Create your account
+          </button>
+        </form>
+        <p>
+          Already have an account?{" "}
+          <Link className="login-link" to="/login">
+            Sign-In.
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CreateAccount;
