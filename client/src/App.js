00b3f2acdf36e09391b3e43138582ae73b78403d
Jassen Kotov <jassen.kotov@gmail.com>
2020-10-31 16:01:00 +0200

    add pages to the app

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,25 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "../src/pages/HomePage/HomePage";
 import LoginPage from "../src/pages/LoginPage/LoginPage";
+import CreateAccount from "../src/pages/CreateAccount/CreateAccount";
+import CreatePost from "../src/pages/CreatePost/CreatePost";
+import ReadPost from "../src/pages/ReadPost/ReadPost";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Switch>
-        <Route path="/login">
+        <Route path="/readPost">
+            <ReadPost />
+          </Route>
+        <Route path="/createPost">
+            <CreatePost />
+          </Route>
+        <Route path="/createAccount">
+            <CreateAccount />
+          </Route>
+          <Route path="/login">
             <LoginPage />
           </Route>
           <Route path="/">
