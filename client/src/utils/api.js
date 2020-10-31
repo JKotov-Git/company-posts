855c18cd74a16294a9af245b0cc6ba1449b07418
Jassen Kotov <jassen.kotov@gmail.com>
2020-10-31 22:03:48 +0200

    add create new user account function

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,8 +3,17 @@ import axios from "axios";
 export const getAllPosts = async (url) => {
   try {
     const response = await axios.get(`http://localhost:8000/${url}`);
-    
+
     return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const createUserAccount = async (url) => {
+  try {
+    const response = await axios.post(`http://localhost:8000/${url}`);
+    return response;
   } catch (error) {
     console.log(error);
   }
