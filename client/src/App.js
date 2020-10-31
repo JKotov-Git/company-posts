0efa0a17104d65951bdc0ae4e4e3dc1abaf8eeea
Jassen Kotov <jassen.kotov@gmail.com>
2020-10-31 15:57:29 +0200

    create react app pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "../src/pages/HomePage/HomePage";
+import LoginPage from "../src/pages/LoginPage/LoginPage";
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+        <Route path="/login">
+            <LoginPage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/pages/ReadPost/ReadPost.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ReadPost = () => {
+    return (
+        <div>
+            <h1>Reade Post</h1>
+        </div>
+    )
+}
+
+export default ReadPost
