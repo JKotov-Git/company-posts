d329d7de01af76550d0425ccd169f6903058343a
Jassen Kotov <jassen.kotov@gmail.com>
2020-11-01 11:51:24 +0200

    create read page

--- client/src/pages/ReadPost/ReadPost.js
+++ client/src/pages/ReadPost/ReadPost.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import "./ReadPost.css";
+import { useLocation } from "react-router-dom";
+import PersonIcon from "@material-ui/icons/Person";
 
 const ReadPost = () => {
-    return (
-        <div>
-            <h1>Read Post</h1>
-        </div>
-    )
-}
+  let location = useLocation();
+  return (
+    <div className="post-article">
+      <h1 className="post-article-title">{location.postDetails.title}</h1>
+      <div className="post-article-batch">
+        <p className="post-article-author">
+          <PersonIcon className="post-article-author-icon" />
+          {location.postDetails.author}
+        </p>
+        <p className="post-article-date">{location.postDetails.date}</p>
+      </div>
 
-export default ReadPost
+      <h2 className="post-article-subtitle">{location.postDetails.subTitle}</h2>
+      <div className="post-article-content-container">
+        <p>{location.postDetails.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ReadPost;
