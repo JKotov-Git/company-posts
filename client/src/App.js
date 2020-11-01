68e5cb268b52f6e26656512e8aff0410879a2f74
Jassen Kotov <jassen.kotov@gmail.com>
2020-11-01 12:55:23 +0200

    update sign in message and read post style

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
           <Route path="/readPost">
           <Header />
             <ReadPost />
-          
+            <Footer />
           </Route>
           <Route path="/createPost">
             <Header />
