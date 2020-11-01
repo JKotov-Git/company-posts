7e8341965b3cd54b55d652fbc9d8c0ba7d0a4aaa
Jassen Kotov <jassen.kotov@gmail.com>
2020-11-01 12:47:43 +0200

    update sign in message if you are not logged

--- client/src/components/signInMessage/SignInMessage.js
+++ client/src/components/signInMessage/SignInMessage.js
@@ -1,10 +1,28 @@
 import React from "react";
 import "./SignInMessage.css";
+import { useHistory } from "react-router-dom";
 
 const SignInMessage = () => {
+  let history = useHistory();
+
+  const openLoginPage = (e) => {
+    e.preventDefault();
+    history.push("/login");
+  };
+
+  const openCreateAccountPage = (e) => {
+    e.preventDefault();
+    history.push("/createAccount");
+  };
+
   return (
-    <div>
+    <div className="message-container">
       <h1 className="message">Please sign in to create a post</h1>
+
+      <div className="message-container-options">
+        <button onClick={openLoginPage}>Sign In</button>
+        <button onClick={openCreateAccountPage}>Create Account</button>
+      </div>
     </div>
   );
 };
