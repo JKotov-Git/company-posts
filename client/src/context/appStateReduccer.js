2259767a05e6ec90b456726ff374bc77981ed801
Jassen Kotov <jassen.kotov@gmail.com>
2020-11-01 16:26:43 +0200

    edit app state reducer

--- client/src/context/appStateReduccer.js
+++ client/src/context/appStateReduccer.js
@@ -6,5 +6,8 @@ export const reducer = (state, action) => {
   switch (action.type) {
     case "SIGNIN_USER":
       return { ...state, user: action.userObject };
+
+    default:
+      return { ...state };
   }
 };
