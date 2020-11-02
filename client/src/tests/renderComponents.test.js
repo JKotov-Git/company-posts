7e0b5149dd276dd520eef21ea62168d67e389294
Jassen Kotov <jassen.kotov@gmail.com>
2020-11-02 10:39:38 +0200

    edit render component test

--- client/src/tests/renderComponents.test.js
+++ client/src/tests/renderComponents.test.js
@@ -16,22 +16,22 @@ configure({ adapter: new Adapter() });
 afterEach(cleanup);
 
 describe("render react component ", () => {
-  it("render header", () => {
-    act(() => {
-      const wrapper = mount(
-        <StateProvider initialState={initialState} reducer={reducer}>
-          <App>
-            <Header />
-          </App>
-        </StateProvider>
-      );
-      expect(wrapper.find('[data-testid="headerTitle"]').text()).toEqual(
-        "Company Posts"
-      );
-    });
+  it("render header title", () => {
+    
+
+    const wrapper = mount(
+      <StateProvider initialState={initialState} reducer={reducer}>
+        <App>
+          <Header />
+        </App>
+      </StateProvider>
+    );
+    expect(wrapper.find('[data-testid="headerTitle"]').text()).toEqual(
+      "Company Posts"
+    );
   });
 
-  it("render navbar", () => {
+  it("render navbar elements", () => {
     act(() => {
       const wrapper = mount(
         <StateProvider initialState={initialState} reducer={reducer}>
