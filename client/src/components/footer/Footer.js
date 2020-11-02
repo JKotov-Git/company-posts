e84708f8746b0fad4cadec3558679aaa2eeaa1a0
Jassen Kotov <jassen.kotov@gmail.com>
2020-11-02 13:11:06 +0200

    edit footer

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,20 +1,32 @@
 import React from "react";
 import "./Footer.css";
 
+import { Link } from "react-router-dom";
+import HomeIcon from "@material-ui/icons/Home";
+
 const Footer = () => {
   return (
     <div className="footer-container">
       <div className="footer-company">
-        <h2>Company Posts</h2>
+        <Link to="/" className="links-footer">
+          <HomeIcon className="header-home-icon" />
+          <h4>Company Posts</h4>
+        </Link>
       </div>
       <div className="footer-copyright">
-        <h4>Copyright</h4>
+        <h4>Copyright © 2020</h4>
       </div>
       <div className="footer-links">
         <ul>
-          <li>Posts</li>
-          <li>Users</li>
-          <li>Sign In</li>
+          <Link to="/login" className="links-footer">
+            <li>Sign In</li>
+          </Link>
+          <Link to="/createAccount" className="links-footer">
+            <li>Create an Account</li>
+          </Link>
+          <Link to="/createPost" className="links-footer">
+            <li>Add new Post</li>
+          </Link>
         </ul>
       </div>
     </div>
