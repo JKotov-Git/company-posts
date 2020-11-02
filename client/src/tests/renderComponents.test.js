302527bf6b846531a2dce5a03517119fcc4cbd04
Jassen Kotov <jassen.kotov@gmail.com>
2020-11-02 09:56:53 +0200

    edit render component test file

--- client/src/tests/renderComponents.test.js
+++ client/src/tests/renderComponents.test.js
@@ -1,20 +1,15 @@
 import React from "react";
 import App from "../App";
 import ReactDOM from "react-dom";
+import { configure, shallow, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
 import { initialState, reducer } from "../context/appStateReduccer";
 import StateProvider from "../context/StateProvider";
 
-import { cleanup, render } from "@testing-library/react";
+import { cleanup } from "@testing-library/react";
 describe("render react component ", () => {
-  it("render App component", () => {
-    const renderApp = render(
-      <StateProvider initialState={initialState} reducer={reducer} />
-    );
-
-    const div = document.createElement("div");
-    ReactDOM.render(renderApp, div);
-  });
+  //
 });
 
 afterEach(cleanup);
